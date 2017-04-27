cb3419326651879a37d98968b587da090118fda5
Nazarii Krypiak <nazarkryp@gmail.com>
2017-04-27 03:10:40 +0300

    added toast service

--- src/app/components/posts/posts.controller.js
+++ src/app/components/posts/posts.controller.js
@@ -1,14 +1,34 @@
-(function() {
+(function () {
     'use strict';
 
     angular.module('photocloud')
         .controller('PostsController', PostsController);
 
-    function PostsController() {
+    PostsController.$inject = ['postService'];
+
+    function PostsController(postService) {
         var vm = this;
 
-        vm.$onInit = function() {
-            console.log('home');
+        vm.data = {
+            posts: [],
+            offset: 1
+        };
+
+        vm.isLoading = false;
+
+        function getPosts() {
+            vm.isLoading = true;
+
+            postService.getPosts()
+                .then(function (response) {
+                    vm.isLoading = false;
+                }, function (error) {
+                    vm.isLoading = false;
+                });
         }
+
+        vm.$onInit = function () {
+            getPosts();
+        };
     }
 })();
