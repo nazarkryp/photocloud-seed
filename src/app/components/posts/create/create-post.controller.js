9648d53523c9227f46cf9b1e002a6044d4a8367a
Nazarii Krypiak <nazarkryp@gmail.com>
2017-05-26 18:56:08 +0300

    create post dialog update

--- src/app/components/posts/create/create-post.controller.js
+++ src/app/components/posts/create/create-post.controller.js
@@ -7,6 +7,35 @@
     CreatePostController.$inject = [];
 
     function CreatePostController() {
+        var vm = this;
+        vm.upload = {
+            attachments: [
+                {
+                    uri: 'https://scontent-fra3-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/c0.134.1080.1080/14033417_178894212519808_1675329581_n.jpg',
+                    isCover: false
+                },
+                {
+                    uri: 'https://scontent-fra3-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/14271989_1774841219441905_279848895_n.jpg',
+                    isCover: false
+                },
+                {
+                    uri: 'https://scontent-fra3-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/18013390_1327946023951549_1021647375392833536_n.jpg',
+                    isCover: false
+                },
+                {
+                    uri: 'https://scontent-fra3-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/c0.135.1080.1080/10724727_438400359617822_497060306_n.jpg',
+                    isCover: true
+                }
+            ],
+            success: false
+        };
 
+        vm.setCover = function (attachment) {
+            vm.preview = attachment.uri;
+        };
+
+        vm.browse = function () {
+
+        };
     }
 })(angular);
